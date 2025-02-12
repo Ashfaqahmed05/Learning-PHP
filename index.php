2406644d222886ae6cd9114139e72af670e88bfc
Ashfaq Ahmed <135797310+Ashfaqahmed05@users.noreply.github.com>
2025-02-12 16:14:24 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    echo "hi how are you"; 
+    echo "<br/>";
+    echo 20 + 15;
+
+
+    $name = "Ashfaq";
+
+    $Name = "Areeb";
+
+    $NAME = "abbas";
+
+    echo "<br/>";
+
+    define("Data", "ali, Ahmed, Tariq");
+
+    echo Data;
+
+    echo  "<h1>Hi everyon I'm h1 and my name is $name</h1>" ;
+    echo "<br/>";
+    echo $Name;
+    echo "<br/>";
+
+    echo $NAME;
+
+?>
+
+<h1>
+    Hello this is H1 and I'm <?php echo $name ?>
+</h1>
